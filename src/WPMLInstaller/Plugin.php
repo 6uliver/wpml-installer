33356f5e30bdc877ab44f4a5d1d842544abde633
Roland Németh <6uliver@gmail.com>
2017-12-02 00:12:11 +0100

    Set dummy distUrl

--- src/WPMLInstaller/Plugin.php
+++ src/WPMLInstaller/Plugin.php
@@ -1,12 +1,22 @@
 <?php
+
 namespace WPMLInstaller;
 
 use Composer\Composer;
+use Composer\DependencyResolver\Operation\OperationInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
 use Composer\Plugin\PluginInterface;
 
-class Plugin implements PluginInterface
+class Plugin implements PluginInterface, EventSubscriberInterface
 {
+    const WPML_PACKAGE_NAME = "wpml/multilingual-cms";
+    const WPML_BASE_URL = "https://wpml.org/";
+    const WPML_DOWNLOAD_ID = "6088";
+
     /**
      * Apply plugin modifications to Composer
      *
@@ -15,6 +25,67 @@ class Plugin implements PluginInterface
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        // TODO: Implement activate() method.
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     * * The method name to call (priority defaults to 0)
+     * * An array composed of the method name to call and the priority
+     * * An array of arrays composed of the method names to call and respective
+     *   priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     * * array('eventName' => 'methodName')
+     * * array('eventName' => array('methodName', $priority))
+     * * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            PackageEvents::PRE_PACKAGE_INSTALL => 'updateDistUrl'
+        ];
+    }
+
+    protected static function getPackage(OperationInterface $operation)
+    {
+        if ($operation->getJobType() === 'update') {
+            return $operation->getTargetPackage();
+        } else {
+            return $operation->getPackage();
+        }
+    }
+
+    public function updateDistUrl(PackageEvent $event)
+    {
+        $package = self::getPackage($event->getOperation());
+
+        if ($package->getName() === self::WPML_PACKAGE_NAME) {
+            $package->setDistUrl($this->getDistUrl($package));
+        }
+    }
+
+    protected function getUserId()
+    {
+        return 'dummyUserId';
+    }
+
+    protected function getSubscriptionKey()
+    {
+        return 'dummySubscriptionKey';
+    }
+
+    protected function getDistUrl(PackageInterface $package)
+    {
+        return self::WPML_BASE_URL .
+            '?download=' . self::WPML_DOWNLOAD_ID .
+            '&user_id=' . $this->getUserId() .
+            '&subscription_key=' . $this->getSubscriptionKey() .
+            '&version=' . $package->getPrettyVersion();
     }
 }
--- tests/WPMLInstaller/PluginTest.php
+++ tests/WPMLInstaller/PluginTest.php
@@ -1,15 +1,81 @@
 <?php
 namespace WPMLInstaller\Test;
 
+use Composer\Installer\PackageEvent;
 use WPMLInstaller\Plugin;
 
 class PluginTest extends \PHPUnit_Framework_TestCase
 {
+    protected $plugin;
+
+    protected function setUp()
+    {
+        $this->plugin = new Plugin();
+    }
+
     public function testImplementsPluginInterface()
     {
         $this->assertInstanceOf(
             'Composer\Plugin\PluginInterface',
-            new Plugin()
+            $this->plugin
         );
+    }
+
+    public function testUrlCreatedCorrectly()
+    {
+        $package = $this
+            ->getMockBuilder('Composer\Package\PackageInterface')
+            ->setMethods([
+                'getName',
+                'getPrettyVersion',
+                'getDistUrl',
+                'setDistUrl'
+            ])
+            ->getMockForAbstractClass();
+
+        $package
+            ->expects($this->once())
+            ->method('getName')
+            ->willReturn("wpml/multilingual-cms");
+        $package
+            ->expects($this->once())
+            ->method('setDistUrl')
+            ->with('https://wpml.org/' .
+                '?download=6088' .
+                '&user_id=dummyUserId' .
+                '&subscription_key=dummySubscriptionKey' .
+                '&version=1.2.3.4');
+        $package
+            ->expects($this->once())
+            ->method('getPrettyVersion')
+            ->willReturn("1.2.3.4");
+
+        $operationClass =
+            'Composer\DependencyResolver\Operation\UpdateOperation';
+        $operation = $this
+            ->getMockBuilder($operationClass)
+            ->disableOriginalConstructor()
+            ->setMethods(['getJobType', 'getTargetPackage'])
+            ->getMock();
+        $operation
+            ->expects($this->once())
+            ->method('getJobType')
+            ->willReturn('update');
+        $operation
+            ->expects($this->once())
+            ->method('getTargetPackage')
+            ->willReturn($package);
+
+        $packageEvent = $this
+            ->getMockBuilder('Composer\Installer\PackageEvent')
+            ->disableOriginalConstructor()
+            ->setMethods(['getOperation'])
+            ->getMock();
+        $packageEvent
+            ->expects($this->once())
+            ->method('getOperation')
+            ->willReturn($operation);
+
+        $this->plugin->updateDistUrl($packageEvent);
     }
 }
